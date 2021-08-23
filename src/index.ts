41276cae4e371bae50274bb0676f988e6a1887d6
devlaq <worldisblack.1@gmail.com>
2021-08-23 09:45:09 +0900

    Rewrited codes

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,15 @@
-import { Discord } from './lib/deps.ts';
-import { EventManager } from "./lib/event.ts";
+import { Config } from "./config.ts";
+import { Discord } from "./deps.ts";
+import { Vars } from "./vars.ts";
 
-class Index {
+const client = new Discord.CommandClient({
+    prefix: 'r',
+    owners: [
+        '872282485097111572'
+    ]
+});
 
-    public client: Discord.CommandClient;
-    
-    public eventManager: EventManager;
-    
-    public constructor() {
-        this.client = new Discord.CommandClient({
-            prefix: 'r'
-        });
-        this.eventManager = new EventManager();
-    }
-
-    public registerEvents(path: string) {
-        this.eventManager.loadEvents(this.client, path);
-    }
-
-    public registerCommands(path: string): number {
-        let cnt = 0;
-        console.log(path);
-        for(const file of Deno.readDirSync(path)) {
-            console.log(file.name);
-			if(file.isFile && file.name.endsWith('.ts')) {
-				import(`${path}/${file.name}`).then((module) => {
-                    console.log(module);
-                    this.client.commands.add(module.default);
-                    cnt++;
-                });
-			}
-		}
-        return cnt;
-    }
-
-    public login(token: string) {
-        this.client.connect(token, Discord.Intents.None).then(() => console.log(`Loggin success\nid: ${this.client.user?.id}, user: ${this.client.user?.tag}`));
-    }
-
-}
-
-const index = new Index();
-index.registerEvents('/home/devlaq/Develop/Sourcebot/src/events');
-const commands = index.registerCommands('/home/devlaq/Develop/Sourcebot/src/commands');
-index.login('ODc3NzI3NzA3MjM5MTcwMTAy.YR21oA.xD3GUz-BgKAHmOF118ApxvqN3Dk');
-//이전 토큰 ODY1MzkxMzc2ODQ4NzgxMzIz.YPDUhg.vgRNMLTAuyNdAkx7xk4AIhx--fg
+const config = new Config(Vars.defaultConfig, './config.json');
+const result = config.load();
+console.log(result);
+console.log(config.data);
