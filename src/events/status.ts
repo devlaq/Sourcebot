f2b8d710f32bce0d64ca3ff408ecadff6c67e3f9
devlaq <worldisblack.1@gmail.com>
2021-08-25 18:05:39 +0900

    Working at status updater

--- src/events/status.ts
+++ src/events/status.ts
@@ -1,11 +1,22 @@
 import { Discord } from "../deps.ts";
-import { Listener } from "../index.ts";
+import { Index, Listener } from "../index.ts";
+import { Vars } from "../vars.ts";
 
 class Status extends Listener {
     event: keyof Discord.ClientEvents = 'ready';
     once = false;
-    listen(args: Discord.ClientEvents['ready']) {
 
+    listen(args: Discord.ClientEvents['ready']) {
+        setInterval(this.update, Index.instance.config.data?.statusDelay as number);
+    }
+    update() {
+        if(Vars.statusMessageIndex == (Index.instance.config.data?.statusMessage as string[]).length) Vars.statusMessageIndex = 0;
+        const message = (Index.instance.config.data?.statusMessage as string[])[Vars.statusMessageIndex++];
+        Index.instance.client.presence.setStatus('online').setActivity({
+            name: message,
+            type: 'CUSTOM_STATUS',
+        })
+        console.log(message);
     }
 }
 
