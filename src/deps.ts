c6b97e33157b8d7cf52a8a159d2e9408ea350345
devlaq <worldisblack.1@gmail.com>
2021-09-07 18:26:11 +0900

    Update Harmony to v2.1.2

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,3 @@
-export * as Discord from 'https://deno.land/x/harmony@v2.0.0-rc4/mod.ts';
+export * as Discord from 'https://deno.land/x/harmony@v2.1.2/mod.ts';
 export * as Path from 'https://deno.land/std@0.106.0/path/mod.ts';
+export * as LavaDeno from 'https://deno.land/x/lavadeno@3.2.2/mod.ts';
