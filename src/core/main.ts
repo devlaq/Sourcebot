97323a81261c1a2e27caf4f72336e6469d156564
devlaq <worldisblack.1@gmail.com>
2021-09-21 23:58:26 +0900

    index.ts moved to src/core index.ts renamed to main.ts class Index renamed to Main

--- src/core/main.ts
+++ src/core/main.ts
@@ -1,31 +1,32 @@
-import { Config } from "./config.ts";
-import { Discord, Path } from "./deps.ts";
+import { Config } from "../util/config/config.ts";
+import { Discord, Path } from "../deps.ts";
 import { Vars } from "./vars.ts";
 
-class Index {
+class Main {
 
-    public config: Config;
+    public config: Config<typeof Vars.defaultBotConfig>;
     public client: Discord.CommandClient;
     public listeners: Listener[] = [];
 
-    public static instance: Index;
+    public static instance: Main;
 
     public constructor(configPath: string | URL) {
-        Index.instance = this;
-        this.config = new Config(Vars.defaultConfig, configPath);
+        Main.instance = this;
+        this.config = new Config(Vars.defaultBotConfig, configPath);
         this.loadConfig();
 
+
         this.client = new Discord.CommandClient({
-            token: this.config.data?.token as string,
-            prefix: this.config.data?.prefix as string,
-            owners: this.config.data?.owners as string[],
+            token: this.config.data.token,
+            prefix: this.config.data.prefix,
+            owners: this.config.data.owners,
             intents: Discord.Intents.None
         });
     }
 
     public loadConfig() {
         const result = this.config.load();
-        if(result == 'loadFailed' || result == 'noDefault' || this.config.data == undefined) {
+        if(result == 'loadFailed'  || this.config.data == undefined) {
             console.log('Failed to load config...');
             console.log('Input valid default to load config successfully');
             Deno.exit();
@@ -33,13 +34,13 @@ class Index {
     }
 
     public async loadCommands() {
-        await this.client.commands.loader.loadDirectory(this.config.data?.commandDirectory as string);
+        await this.client.commands.loader.loadDirectory(this.config.data?.commandDirectory);
     }
 
     public async loadEvents() {
         for(const i of Deno.readDirSync(this.config.data?.eventDirectory as string)) {
             if(i.name.endsWith('.ts')) {
-                const mod = await import(`file:///${Path.join(Deno.cwd(), this.config.data?.eventDirectory as string, i.name)}`);
+                const mod = await import(`file:///${Path.join(Deno.cwd(), this.config.data?.eventDirectory, i.name)}`);
                 const listener = new mod.default() as Listener;
                 this.listeners.push(listener);
                 if(listener.once) {
@@ -82,11 +83,13 @@ class Listener {
     public listen(args: Discord.ClientEvents[keyof Discord.ClientEvents]): any {}
 }
 
-const index = new Index('./config.json');
+const index = new Main('./config.json');
+
 index.loadCommands();
 index.loadEvents();
 index.addCategories();
 index.connect();
 
 
-export { Index, Listener };
+
+export { Main, Listener };
