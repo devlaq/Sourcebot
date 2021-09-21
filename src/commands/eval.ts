ed8605ebbb369f3282fdd68362844f69666caff0
devlaq <worldisblack.1@gmail.com>
2021-09-22 00:00:40 +0900

    Update eval.ts

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,5 +1,4 @@
 import { Discord } from "../deps.ts";
-import { Index } from "../index.ts";
 
 class Eval extends Discord.Command {
     name = 'eval';
@@ -12,7 +11,7 @@ class Eval extends Discord.Command {
     execute(context: Discord.CommandContext) {
         if(context.rawArgs.length < 1) {
             const embed = new Discord.Embed()
-            .setTitle('코드 평가')
+            .setTitle('코드 평가 실패')
             .setDescription('코드를 원격으로 실행하려면 인수를 주어야합니다!')
             .setColor('RED');
 
@@ -22,14 +21,14 @@ class Eval extends Discord.Command {
             try {
                 let result = eval(target);
                 const embed = new Discord.Embed()
-                .setTitle('코드 평가')
+                .setTitle('코드 평가 성공')
                 .setDescription(`코드 실행을 완료했습니다.\n\nResult:\n\`${result}\``)
                 .setColor('GREEN');
 
                 context.channel.send(embed);
             } catch(err) {
                 const embed = new Discord.Embed()
-                .setTitle('코드 평가')
+                .setTitle('코드 평가 실패')
                 .setDescription(`코드 실행에 실패했거나 코드 실행 도중 에러가 발생했습니다.\n\nError:\n\`${err}\``)
                 .setColor('RED');
 
