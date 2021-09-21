5edcdec1747f72b8f071bf83e5c6835ba92a2971
devlaq <worldisblack.1@gmail.com>
2021-09-22 00:04:57 +0900

    Added command guild/owner only handler

--- src/events/commandGuildOnly.ts
+++ src/events/commandGuildOnly.ts
@@ -1,17 +1,18 @@
 import { Discord } from "../deps.ts";
-import { Listener } from "../index.ts";
+import { Listener } from "../core/main.ts";
 
-class CommandOwnerOnly extends Listener {
-    event: keyof Discord.ClientEvents = 'commandOwnerOnly';
+class CommandGuildOnly extends Listener {
+    event: keyof Discord.ClientEvents = 'commandGuildOnly';
     once = false;
-    listen(args: Discord.ClientEvents['commandOwnerOnly']) {
+    listen(args: Discord.ClientEvents['commandGuildOnly']) {
         const context = args[0];
         const embed = new Discord.Embed()
         .setTitle('Access Denied')
-        .setDescription(`${context.author.mention}, 이 명령어는 개발자만 사용 가능한 명령어입니다.`)
+        .setDescription(`${context.author.mention}, 이 명령어는 서버에서만 사용 가능한 명령어입니다.`)
         .setColor('RED');
         context.channel.send(embed);
+        context.message.type
     }
 }
 
-export default CommandOwnerOnly;
+export default CommandGuildOnly;
