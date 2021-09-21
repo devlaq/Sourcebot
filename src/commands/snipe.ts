ad14b547aedd7fd2248c955d8d124fb4feaeb2fc
devlaq <worldisblack.1@gmail.com>
2021-09-22 00:03:33 +0900

    Added snipe command

---
+++ src/commands/snipe.ts
@@ -0,0 +1,12 @@
+import { Discord } from "../deps.ts";
+
+class Snipe extends Discord.Command {
+    name = 'snipe';
+    category = 'admin';
+
+    execute(context: Discord.CommandContext) {
+        
+    }
+}
+
+export default Snipe;
